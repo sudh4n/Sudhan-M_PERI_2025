3463478fa0dc9dfffbe33f7c909992392874173d
Sudhan M <98807138+sudh4n@users.noreply.github.com>
2024-02-04 20:45:28 +0530

    Question 2 in Python

--- 24-01-2024/Question 2.py
+++ 24-01-2024/Question 2.py
@@ -43,12 +43,39 @@ image
 Because he managed to supply all four bases with a single supply drop, we print  as our answer."""
 
 code:
-def handshakes(n):
-    return n * (n - 1) // 2
 
+#!/bin/python3
 
-test_cases = int(input())
-for _ in range(test_cases):
-    attendees = int(input())
-    result = handshakes(attendees)
-    print(result)
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gameWithCells' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def gameWithCells(n, m):
+    # Write your code here
+    return (m//2+m%2)*(n//2+n%2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = gameWithCells(n, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
