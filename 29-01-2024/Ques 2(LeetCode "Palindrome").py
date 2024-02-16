b5d4385b0743144b1c1e555ee35da5fee9d8568d
Sudhan M <98807138+sudh4n@users.noreply.github.com>
2024-02-16 21:30:13 +0530

    Ques 2(LeetCode "Palindrome").py

--- 29-01-2024/Ques 2(LeetCode "Palindrome").py
+++ 29-01-2024/Ques 2(LeetCode "Palindrome").py
@@ -28,15 +28,6 @@ Constraints:
 #Solution
 
 class Solution:
-  def isPalindrome(self, x: int) -> bool:
-    if x < 0:
-      return False
-
-    rev = 0
-    y = x
-
-    while y:
-      rev = rev * 10 + y % 10
-      y //= 10
-
-    return rev == x
+    def isPalindrome(self, x: int) -> bool:
+        c=str(x)
+        return c == c[::-1]
